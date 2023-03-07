076f5a1185914f70c84a39503342941cc955978d
Filip Vaca <vac39328@spseol.cz>
2023-03-07 13:18:55 +0100

    zkouska

---
+++ výpis a graf.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+from tkinter import filedialog
+ 
+import pylab as pl
+
+class Aplikation(tk.Tk):
+    name = "Foo"
+
+    def __init__(self):
+        super().__init__(className=self.name)
+        self.title(self.name)
+        self.bind("<Escape>", self.quit)
+        self.lbl=tk.Label(self, text="Vyber soubor")
+        self.lbl.pack()
+        self.chooseBtn = tk.Button(self, text="...", command=self.chooseBtn)
+        self.chooseBtn.pack()
+        self.showBtn = tk.Button(self, text="Show", command=self.showBtn)
+        self.showBtn.pack()
+        self.quitBtn = tk.Button(self, text="Quit", command=self.quitBtn)
+        self.quitBtn.pack()
+
+    def choose(self):
+        self.filename = filedialog.askopenfile()
+        self.lbl.config(text=self.filename)
+
+    def show(self):
+        if not self.filename:
+            return
+        axisx = []
+        axisy = []
+        with open(self.filename, "r") as f:
+            while line := f.readline():
+                x, y = line.split()
+                axisx.append(float(x))
+                axisy.append(float(y))
+            pl.plot(axisx, axisy)
+            pl.show()
+    
+    def quit(self, event=None):
+        super().quit()
+
+app = Aplikation
+app.mainloop()
+
