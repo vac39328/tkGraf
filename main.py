a965afb0d6790a6c45c9190735f57280c2cb3bcc
Filip Vaca <vac39328@spseol.cz>
2023-03-14 14:02:18 +0100

    graf2.0

---
+++ main.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+from os.path import basename, splitext
+import tkinter as tk
+from tkinter import filedialog
+import pylab as pl
+# from tkinter import ttk
+
+
+class MyEntry(tk.Entry):
+    def __init__(self, master=None, cnf={}, **kw):
+        super().__init__(master, cnf, **kw)
+
+        if not "textvariable" in kw:
+            self.variable = tk.StringVar()
+            self.config(textvariable=self.variable)
+        else:
+            self.variable = kw["textvariable"]
+
+    @property
+    def value(self):
+        return self.variable.get()
+
+    @value.setter
+    def value(self, new: str):
+        self.variable.set(new)
+
+
+
+class Application(tk.Tk):
+    name = basename(splitext(basename(__file__.capitalize()))[0])
+    name = "Foo"
+
+    def __init__(self):
+        super().__init__(className=self.name)
+        self.title(self.name)
+        self.bind("<Escape>", self.quit)
+        self.lbl = tk.Label(self, text="Hello World")
+        self.lbl.pack()
+
+        self.fileFrame = tk.LabelFrame(self, text='Soubor')
+        self.fileFrame.pack(padx=5, pady=5, fill='x')
+        self.fileEntry = MyEntry(self.fileFrame)
+        self.fileEntry.pack(fill='x')
+        self.fileBtn = tk.Button(self.fileFrame, text ='...', command=self.choosefile)
+        self.fileBtn.pack(anchor='e')
+
+        self.dataVar = tk.StringVar()
+        self.rowRadio = tk.Radiobutton(self.fileFrame, text='Data jsou v řádcích!', variable=self.dataVar, value='ROW')
+        self.rowRadio.pack(anchor='w')
+        self.columnRadio = tk.Radiobutton(self.fileFrame, text='Data jsou ve sloupcích!', variable=self.dataVar, value='COLUMN')
+        self.columnRadio.pack(anchor='w')
+
+        self.Btn = tk.Button(self, text='Kresli', command=self.plot)
+        self.Btn.pack(fill='x')
+        self.btn = tk.Button(self, text='Quit', command=self.quit)
+        self.btn.pack()
+
+
+    def choosefile(self):
+        path= filedialog.askopenfilename()
+        self.fileEntry.value = path
+    
+    def plot(self):
+        with open(self.fileEntry.value) as f:
+            if self.dataVar.get()== 'ROW':
+                line = f.readline()
+                x = line.split(';')
+                line = f.readline()
+                y = line.split(';')
+                x = [ float(i.replace(',','.')) for i in x]
+                y = [ float(i.replace(',','.')) for i in y]   
+        pl.plot(x, y)
+        pl.show()
+
+    def quit(self, event=None):
+        super().quit()
+
+app = Application()
+app.mainloop()
